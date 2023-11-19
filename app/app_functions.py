dfd5e6aa11552c914bc912c197ed5e7afbbb62d3
Allan de Oliveira Almeida <arumeidaaran@outlook.com>
2023-11-18 22:24:09 -0300

    Display table data from csv file to webpage

--- app/app_functions.py
+++ app/app_functions.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from py_rpautom import python_utils as pyutils
 from routes import index
 
 
 def application(debug: bool = True):
+    project_path = pyutils.coletar_arvore_caminho(caminho=__name__)
+    templates_path = '\\'.join((project_path, 'templates'))
+
     app = Flask(import_name=__name__)
     app.debug = debug
+    app.template_folder = templates_path
 
     index.index(app=app)
 
--- routes/index.py
+++ routes/index.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from py_rpautom.python_utils import abrir_arquivo_texto
+
 
 def index(app: Flask):
     @app.route('/')
-    def route():
+    def _():
         return route_content()
-
-    return route()
 
 
 def route_content():
@@ -15,4 +14,14 @@ def route_content():
         encoding='utf8',
     ).splitlines()
 
-    return file_content
+    file_content_temp = [line.split(';') for line in file_content]
+
+    file_content_temp_2 = []
+    for line in file_content_temp:
+        file_content_temp_2.append(
+            [collumn.replace('"', '') for collumn in line]
+        )
+
+    file_content = file_content_temp_2
+
+    return render_template('index.html', data=file_content)
